12939105ad18b92cf2e4f51d8c6a4edd9a6c8a46
DuQian <duqian2010@gmail.com>
2021-07-17 15:32:50 +0800

    AndroidUI--实现页面无限滚动

--- app/src/main/java/site/duqian/android_ui/fragment/ViewPagerFragment.kt
+++ app/src/main/java/site/duqian/android_ui/fragment/ViewPagerFragment.kt
@@ -3,12 +3,14 @@ package site.duqian.android_ui.fragment
 import android.view.View
 import android.widget.RelativeLayout
 import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import site.duqian.android_ui.view.pageTransformer.ScaleInTransformer
 import site.duqian.android_ui.R
 import site.duqian.android_ui.utils.dp
-import site.duqian.android_ui.view.pageTransformer.ZoomOutPageTransformer
+import site.duqian.android_ui.view.pageTransformer.ScaleInTransformer
+
 
 /**
  * description:ViewPager转场效果
@@ -18,7 +20,7 @@ import site.duqian.android_ui.view.pageTransformer.ZoomOutPageTransformer
 class ViewPagerFragment : BaseFragment() {
     private lateinit var mTvTitle: View
     private lateinit var mViewPager: ViewPager2
-    private var fragmentList: MutableList<BaseFragment>? = null
+    private var fragmentList: MutableList<BaseFragment> = ArrayList()
 
     companion object {
         fun newInstance() = ViewPagerFragment()
@@ -34,28 +36,63 @@ class ViewPagerFragment : BaseFragment() {
     }
 
     override fun initData() {
-        fragmentList = ArrayList()
-        fragmentList!!.add(AnimationFragment())
-        fragmentList!!.add(ImageEffectFragment())
-        fragmentList!!.add(ImageEffectFragment())
-        fragmentList!!.add(ImageEffectFragment())
-        fragmentList!!.add(ProgressFragment())
+        fragmentList.clear()
+        fragmentList.also {
+            it.add(AnimationFragment())
+            it.add(ImageEffectFragment())
+            it.add(ImageEffectFragment())
+            it.add(ImageEffectFragment())
+            it.add(ProgressFragment())
+        }
 
         val adapter: MyFragmentStateAdapter? =
-            activity?.let { MyFragmentStateAdapter(it, fragmentList!!) }
+            activity?.let { MyFragmentStateAdapter(it, fragmentList) }
         mViewPager.adapter = adapter
         mViewPager.setPageTransformer(ScaleInTransformer())//设置页面切换的动画
-        //mViewPager.setPageTransformer(ZoomOutPageTransformer())//设置页面切换的动画
         mViewPager.isUserInputEnabled = true //false ，禁用页面滑动
 
-        mViewPager.currentItem = 2
-        mViewPager.offscreenPageLimit = 3
+        mViewPager.currentItem = fragmentList.size / 2
+        mViewPager.offscreenPageLimit = 4
 
+        //设置左右padding
         val lp = mViewPager.layoutParams as RelativeLayout.LayoutParams
         mViewPager.layoutParams = lp
         val margin2 = 20.dp.toInt()
-        mViewPager.setPadding(margin2,10,margin2,10)
+        mViewPager.setPadding(margin2, 10, margin2, 10)
         mViewPager.clipToPadding = false
+
+        //设置监听实现无线滚动
+        addPagerScrollListener()
+    }
+
+    private fun addPagerScrollListener() {
+        mViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            var currentPosition = 0
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                currentPosition = position
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                // SCROLL_STATE_IDLE 页面完全展现，没有动画,防止页面闪烁！
+                if (state != ViewPager2.SCROLL_STATE_IDLE && fragmentList.size < 2) return
+
+                // 当视图在第一个时，将页面号设置为图片的最后一张。
+                if (currentPosition == 0) {
+                    mViewPager.setCurrentItem(fragmentList.size - 2, false)
+                } else if (currentPosition == fragmentList.size - 1) {
+                    // 当视图在最后一个是,将页面号设置为图片的第一张。
+                    mViewPager.setCurrentItem(1, false)
+                }
+            }
+        })
     }
 
     class MyFragmentStateAdapter(
