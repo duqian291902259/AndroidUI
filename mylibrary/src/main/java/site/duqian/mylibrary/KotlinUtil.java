0b1d6aa7c629cabab58310c14c64195103131268
DuQian <duqian2010@gmail.com>
2021-09-19 00:09:55 +0800

    AndroidUI--add library

---
+++ mylibrary/src/main/java/site/duqian/mylibrary/KotlinUtil.java
@@ -0,0 +1,13 @@
+package site.duqian.mylibrary;
+
+/**
+ * Description:
+ *
+ * @author 杜小菜, Created on 2021/9/18 - 23:35.
+ * E-mail:duqian2010@gmail.com
+ */
+class KotlinUtil {
+    public static int testAdd(int a) {
+        return a + 100000;
+    }
+}
